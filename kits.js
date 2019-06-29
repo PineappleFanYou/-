88e7937883577e8cf500f35f44583f7b323904af
范优 <876255983@qq.com>
2019-06-30 06:40:05 +0800

    forEach的用法

--- kits.js
+++ kits.js
@@ -5,3 +5,15 @@
 function randomInt(n,m) {
     return Math.floor(Math.random() * (m - n + 1) + n);
 };
+
+
+// 数组的遍历的forEach方法
+// 作用： 用于遍历数组
+// 参数： 是一个函数，这个函数有两个形参，第一个形参代表了数组里面的每个元素，第二个形参代表了每个索引
+// 返回值 ：返回值是undefined
+// 例如：
+// var arr = [1,2,3,4,5];
+// var sum = 0;
+// var res = arr.forEach(function(e,i){
+//   sum += e;
+// };
