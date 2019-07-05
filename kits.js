83a6684b8d206856597f89b682efa2a9860b6124
范优 <876255983@qq.com>
2019-07-05 10:10:38 +0800

    封装时间

--- kits.js
+++ kits.js
@@ -2,7 +2,7 @@
 
 
 // 封装一个可以获得随机区间的整数的函数
-function randomInt(n,m) {
+function randomInt(n, m) {
     return Math.floor(Math.random() * (m - n + 1) + n);
 };
 
@@ -12,10 +12,10 @@ function randomInt(n,m) {
 // 参数： 是一个函数，这个函数有两个形参，第一个形参代表了数组里面的每个元素，第二个形参代表了每个索引
 // 返回值 ：返回值是undefined
 // 例如：
-var arr = [1,2,3,4,5];
+var arr = [1, 2, 3, 4, 5];
 var sum = 0;
-var res = arr.forEach(function(e,i){
-  sum += e;
+var res = arr.forEach(function (e, i) {
+    sum += e;
 });
 
 
@@ -26,22 +26,41 @@ var res = arr.forEach(function(e,i){
 // 返回值：是一个数组，里面有所有满足条件的元素
 // 例如：
 // 筛选出数组中满足条件的元素 ,条件就是 元素 > 2000
-var arr = [3000,5000,100,80,99,2500];
-var arr2 = arr.filter(function(e,i){
-	return e > 2000;
+var arr = [3000, 5000, 100, 80, 99, 2500];
+var arr2 = arr.filter(function (e, i) {
+    return e > 2000;
 })
 
 
-   // 求最大值 并封装函数
+// 求最大值 并封装函数
 
-function max(n,m,a) {
-        var max = arr[0];
-        for (var i = 0; i < arr.length; i++) {
-            if (max < arr[i]) {
-                max = arr[i]
-            }
+function max(n, m, a) {
+    var max = arr[0];
+    for (var i = 0; i < arr.length; i++) {
+        if (max < arr[i]) {
+            max = arr[i]
         }
-        return max;
-    };
-    arr = [1, 9, 3, 7, 5]
-    console.log(max(arr)) */
+    }
+    return max;
+};
+arr = [1, 9, 3, 7, 5]
+console.log(max(arr))
+
+
+
+// 封装时间
+function times() {
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    month = month < 10 ? '0' + month : month;
+    day = day < 10 ? '0' + day : day;
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    return year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds;
+}
