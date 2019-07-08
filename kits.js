9eb290fb4de21286f18fb8891ed18cfeff49c2d5
范优 <876255983@qq.com>
2019-07-09 07:17:14 +0800

    封装一个可以随机生成唯一id的方法

--- kits.js
+++ kits.js
@@ -88,3 +88,18 @@ function fn () {
         }
     }
 }
+
+
+
+/**
+ * 封装的是一个可以生成唯一id的方法
+ */
+kits.primaryKey = function () {
+    // 我们通过时间戳 + 大范围的随机数来生成id
+    let now = Date.now(); //得到是从1970年到现在为止的总的毫秒数
+    // 为了防止在1毫秒之内生成的id有多个，再次加上一个大范围的随机数
+    let r = kits.randomInt(100000, 999999);
+    // console.log(r);
+    // 把两个得到的结果，拼接起来
+    return now + '' + r;
+}
